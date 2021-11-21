bf7c476dd603d307072edca175b25a396b0e1e42
Nomeleel <choonlay@outlook.com>
2021-11-21 23:14:57 +0800

    Svga preview process...

--- src/webview/index.ts
+++ src/webview/index.ts
@@ -8,17 +8,21 @@ async function load(svgaBuffer: ArrayBuffer) {
   player = new Player('#canvas');
   await player.mount(svgaData);
 
+  let processElement : HTMLHRElement = document.getElementById('process') as HTMLHRElement;
+  player.$on('process', () => {
+    processElement.innerText = `${player.currentFrame + 1} / ${player.totalFramesCount}`;
+  });
+
   let videoItem = player.videoItem;
   if (videoItem) {
     let videoSize = videoItem.videoSize;
     let sizeElement : HTMLHRElement = document.getElementById('size') as HTMLHRElement;
     sizeElement.innerText += `    ${videoSize.width}  ×  ${videoSize.height}`;
 
-    let frameElement : HTMLHRElement = document.getElementById('frame') as HTMLHRElement;
-    frameElement.innerText += '   ' + videoItem.frames;
-
     let gridElement = document.getElementById('grid');
+    let imageCount = 0;
     for (const key in videoItem.images) {
+      imageCount++;
       let item = document.createElement('div');
       item.setAttribute('class', 'item');
       let image: HTMLImageElement = document.createElement('img') as HTMLImageElement;
@@ -32,6 +36,9 @@ async function load(svgaBuffer: ArrayBuffer) {
       item.appendChild(label);
       gridElement.appendChild(item);
     }
+
+    let countElement : HTMLHRElement = document.getElementById('count') as HTMLHRElement;
+    countElement.innerText += '   ' + imageCount;
   }
   console.log(player);
 }
